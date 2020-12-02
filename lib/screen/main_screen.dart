f817284337a69aebbb341e41b2bee581631df34d
Zoey Child <imperialoctopus@gmail.com>
2020-12-02 14:45:29 +0000

    Add main screen

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
+/// Main app screen with counter.
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Top'),
+            Text('Middle'),
+            Text('Bottom'),
+          ],
+        ),
+      ),
+    );
   }
 }
