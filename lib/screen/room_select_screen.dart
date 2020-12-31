c12826153429ab00492c5fab1eb3bab0acd9a92c
Zoey Child <imperialoctopus@gmail.com>
2020-12-31 22:12:45 +0000

    Add register new event button

--- lib/screen/room_select_screen.dart
+++ lib/screen/room_select_screen.dart
@@ -35,50 +35,86 @@ class _RoomSelectScreenState extends State<RoomSelectScreen> {
             builder: (context, state) => Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  widget.title,
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.headline1,
-                ),
-                Container(height: 20),
-                Container(
-                  width: 350,
-                  child: TextFormField(
-                    controller: roomCodeController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        borderSide: BorderSide(),
-                      ),
-                    ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    widget.title,
                     textAlign: TextAlign.center,
-                    inputFormatters: [
-                      LowercaseTextFormatter(),
-                    ],
-                    maxLines: null,
-                    style: Theme.of(context).textTheme.headline4,
-                    onChanged: (string) =>
-                        setState(() => _submitEnabled = (string != '')),
+                    style: Theme.of(context).textTheme.headline1,
                   ),
                 ),
-                if (state is RoomLoadError) Container(height: 20),
-                if (state is RoomLoadError)
-                  Text(
-                    'failed to join event',
-                    style: Theme.of(context).textTheme.headline4,
-                  ),
-                Container(height: 20),
-                (state is LoadingRoom || state is InRoom)
-                    ? CircularProgressIndicator()
-                    : ElevatedButton(
-                        child: Text('Join Event'),
-                        onPressed: _submitEnabled
-                            ? () {
-                                BlocProvider.of<RoomBloc>(context).add(
-                                    EnterRoomEvent(roomCodeController.text));
-                              }
-                            : null,
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Container(
+                    width: 350,
+                    child: TextFormField(
+                      controller: roomCodeController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(),
+                        ),
                       ),
+                      textAlign: TextAlign.center,
+                      inputFormatters: [
+                        LowercaseTextFormatter(),
+                      ],
+                      maxLines: null,
+                      style: Theme.of(context).textTheme.headline4,
+                      onChanged: (string) =>
+                          setState(() => _submitEnabled = (string != '')),
+                    ),
+                  ),
+                ),
+                if (state is RoomLoadError)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      'failed to join event',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                  ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: (state is LoadingRoom || state is InRoom)
+                      ? CircularProgressIndicator()
+                      : Column(
+                          children: [
+                            ElevatedButton(
+                              child: Text('Join Event'),
+                              onPressed: _submitEnabled
+                                  ? () {
+                                      BlocProvider.of<RoomBloc>(context).add(
+                                          EnterRoomEvent(
+                                              roomCodeController.text));
+                                    }
+                                  : null,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 10),
+                            ),
+                            ElevatedButton(
+                              child: Text('Register New Event'),
+                              onPressed: () => showDialog(
+                                context: context,
+                                builder: (context) => AlertDialog(
+                                  title: Text("Register New Event"),
+                                  content: Text(
+                                      // ignore: lines_longer_than_80_chars
+                                      "To register new events contact imperialoctopus@gmail.com with details of your event."),
+                                  actions: [
+                                    FlatButton(
+                                      child: Text("Close"),
+                                      onPressed: () =>
+                                          Navigator.of(context).pop(),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                ),
               ],
             ),
           ),
